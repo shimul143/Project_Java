31046ff98fae460691df8760d466584dca9c6efc
Rakibul Hasan Shimul <50355564+shimul143@users.noreply.github.com>
2019-07-19 15:33:55 +0900

    Add files via upload

---
+++ Part_1/Hello/src/hello/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hello;
+
+/**
+ *
+ * @author shimul
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        // TODO code application logic here
+    }
+
+}
---
+++ Part_1/MyFirstJava/src/MyFirstJava.java
@@ -0,0 +1,9 @@
+
+public class MyFirstJava
+{
+    public static void main(String args[])
+    {
+        System.out.println("I love my Bangaldesh");
+    }
+
+}
